b30fd3ab07ffa489587e6ffb4c416453ff302cc1
Santtu <santeriq93@gmail.com>
2016-03-10 13:40:18 +0200

    Header for Frequency Controller

---
+++ Frequency Controller.h
@@ -0,0 +1,13 @@
+/*
+ * FreqContrl.h
+ *
+ *  Created on: 10.3.2016
+ *      Author: Santtu
+ */
+
+#ifndef CONTROLLER_FREQCONTRL_H_
+#define CONTROLLER_FREQCONTRL_H_
+
+void SetFansSpeed(int inputSpeed);
+
+#endif /* CONTROLLER_FREQCONTRL_H_ */
